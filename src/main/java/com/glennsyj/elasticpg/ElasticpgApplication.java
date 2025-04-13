db7b41ffa92c94054843d307aa456ed2dda44dea
glenn-syj <glenn.syj@gmail.com>
2025-04-13 23:56:43 +0900

    init: initialize spring boot project with gradle

---
+++ src/main/java/com/glennsyj/elasticpg/ElasticpgApplication.java
@@ -0,0 +1,13 @@
+package com.glennsyj.elasticpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glennsyj/elasticpg/ElasticpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glennsyj.elasticpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
